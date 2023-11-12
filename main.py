525e898aa23cd648dc716f7640aa0cd9dfca6e28
Diogo Laurindo <diogo.laurindo01@gmail.com>
2023-11-12 12:48:43 -0300

    Tiny change at the interface

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     while True:
         name = input("Digite o nome do ator: ")
         path = graph.get_path_from_kevin(name)
-        print(f"\nO número de conexões entre Kevin Bacon e {name} é {len(path)}\nAqui está o caminho entre eles: {path}.\n")
+        print(f"\nO número de conexões entre Kevin Bacon e {name} é {len(path)}.\nAqui está o caminho entre eles: {path}.\n")
         print("O que deseja agora?\na. Buscar outra conexão\nb. Sair\n")
         next_step = input("Selecione 'a' ou 'b': ").lower().strip()
         while next_step != 'a' and next_step != 'b':
