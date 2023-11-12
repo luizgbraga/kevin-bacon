eb90abd416065bccaf723d1bad21aa203ee0d44a
Diogo Laurindo <diogo.laurindo01@gmail.com>
2023-11-12 12:41:53 -0300

    Creating interface and gitignore

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ from graph import Graph
 def main():
     graph = Graph('netflix_titles.csv')
     # graph.display()
-    name = 'Samuel L. Jackson'
-    print(graph.get_path_from_kevin(name))
+    while True:
+        name = input("Digite o nome do ator: ")
+        path = graph.get_path_from_kevin(name)
+        print(f"\nO número de conexões entre Kevin Bacon e {name} é {len(path)}\nAqui está o caminho entre eles: {path}.\n")
+        print("O que deseja agora?\na. Buscar outra conexão\nb. Sair\n")
+        next_step = input("Selecione 'a' ou 'b': ").lower().strip()
+        while next_step != 'a' and next_step != 'b':
+            next_step = input("Selecione uma opção válida: ")
+        if next_step == 'b':
+            break
+        
+            
 
 if __name__ == '__main__':
     main()
